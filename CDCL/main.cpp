395841f8ee27206f10752ef505a567b78d5a9c6f
jiriklepl <jiriklepl@seznam.cz>
2021-04-19 03:58:52 +0200

    update

--- CDCL/main.cpp
+++ CDCL/main.cpp
@@ -23,70 +23,121 @@ struct AdjacencyList;
 struct WatchedList;
 template<typename> class Solver;
 
+struct WatchedList {
+    std::vector<std::vector<Clause<watch_tag>*>> watched_at;
+};
+
 template<>
 class Clause<watch_tag> {
 public:
+    using WatchHandle = std::pair<lit_t, std::size_t>*;
+
+    static constexpr std::size_t get_var(WatchHandle w) { return std::abs(w->first); }
+
+    static constexpr lit_t &get_lit(WatchHandle &w) { return w->first; }
+    static constexpr const lit_t &get_lit(const WatchHandle &w) { return w->first; }
+
+    static constexpr std::size_t &get_pos(WatchHandle &w) { return w->second; }
+    static constexpr const std::size_t &get_pos(const WatchHandle &w) { return w->second; }
+
     Clause() noexcept = default;
-    Clause(lit_t *_start, lit_t *_end, std::vector<std::unordered_set<Clause*>> &watched_at) noexcept
+    Clause(WatchHandle _start, WatchHandle _end, WatchedList &watched_list) noexcept
         : start(_start), end(_end), w1(_start), w2(_end - 1), satisfied(0) {
             assert(start != nullptr);
             assert(end != nullptr);
             assert(w1 != w2);
 
-            watched_at[std::abs(*w1)].emplace(this);
-            watched_at[std::abs(*w2)].emplace(this);
+            w1->second = watched_list.watched_at[get_var(w1)].size();
+            watched_list.watched_at[get_var(w1)].emplace_back(this);
+
+            w2->second = watched_list.watched_at[get_var(w2)].size();
+            watched_list.watched_at[get_var(w2)].emplace_back(this);
+
+            assert(watched_list.watched_at[get_var(w1)][get_pos(w1)] == this);
+            assert(watched_list.watched_at[get_var(w2)][get_pos(w2)] == this);
         }
-    bool update(const std::vector<lit_t> &variables, std::vector<std::unordered_set<Clause*>> &watched_at, lit_t variable) {
+    bool update(const std::vector<lit_t> &variables, WatchedList &watched_list) noexcept {
+        if (variables[get_var(w1)] * get_lit(w1) > 0) {
+            return true;
+        }
+
+        assert(variables[get_var(w2)] * get_lit(w2) < 0);
+
         auto tmp = w2;
-        while (true) {
-            ++w2;
+        for (++w2;;++w2) {
             if (w2 == end) w2 = start;
             if (w2 == tmp) break;
-            else if (variables[std::abs(*w2)] == 0) {
+            else if (variables[get_var(w2)] == 0) {
                 if (w2 == w1) {
                     continue;
                 } else {
-                    assert(variables[std::abs(*w1)] == 0);
-                    watched_at[variable].erase(this);
-                    watched_at[std::abs(*w2)].emplace(this);
+                    assert(variables[get_var(w1)] == 0);
+                    auto &&var_item = watched_list.watched_at[get_var(tmp)];
+                    auto &&this_ref = watched_list.watched_at[get_var(tmp)][get_pos(tmp)];
+                    assert(this_ref == this);
+
+                    if (&this_ref != &var_item.back()) {
+                        std::swap(this_ref, var_item.back());
+                        if(get_var(tmp) == get_var(this_ref->w1)) {
+                            get_pos(this_ref->w1) = get_pos(tmp);
+                        } else {
+                            assert(get_var(tmp) == get_var(this_ref->w2));
+                            get_pos(this_ref->w2) = get_pos(tmp);
+                        }
+
+                        assert(this_ref->w1 != this_ref->w2);
+                        assert(watched_list.watched_at[get_var(this_ref->w1)][get_pos(this_ref->w1)] == this_ref);
+                        assert(watched_list.watched_at[get_var(this_ref->w2)][get_pos(this_ref->w2)] == this_ref);
+                    }
+
+                    var_item.pop_back();
+
+                    get_pos(w2) = watched_list.watched_at[get_var(w2)].size();
+                    watched_list.watched_at[get_var(w2)].emplace_back(this);
                     break;
                 }
-            } else if (variables[std::abs(*w2)] * *w2 > 0) {
+            } else if (variables[get_var(w2)] * get_lit(w2) > 0) {
                 w2 = tmp;
+
+                assert(w1 != w2);
+                assert(watched_list.watched_at[get_var(w1)][get_pos(w1)] == this);
+                assert(watched_list.watched_at[get_var(w2)][get_pos(w2)] == this);
                 return true;
             }
         }
 
         assert(w1 != w2);
-        assert(watched_at[std::abs(*w1)].find(this) != watched_at[std::abs(*w1)].end());
-        assert(watched_at[std::abs(*w2)].find(this) != watched_at[std::abs(*w2)].end());
-        assert((variables[std::abs(*w1)] == 0) || (variables[std::abs(*w2)] != 0));
+        assert(watched_list.watched_at[get_var(w1)][get_pos(w1)] == this);
+        assert(watched_list.watched_at[get_var(w2)][get_pos(w2)] == this);
+        assert((variables[get_var(w1)] == 0) || (variables[get_var(w2)] != 0));
 
         return false;
     }
 
-    lit_t get_watch() const { return *w1; }
-    lit_t snd_watch() const { return *w2; }
-    void assure_watch(lit_t variable) { 
-        if (std::abs(*w2) != variable)
+    const lit_t &get_watch() const noexcept { return get_lit(w1); }
+    const lit_t &snd_watch() const noexcept { return get_lit(w2); }
+
+    void assure_watch(std::size_t variable) noexcept {
+        if (get_var(w2) != variable)
             std::swap(w1, w2);
-        assert(std::abs(*w2) == variable);
+        assert(get_var(w2) == variable);
     }
-    bool is_unit(const std::vector<lit_t> &variables) const {
-        return variables[std::abs(*w1)] == 0 && variables[std::abs(*w2)] != 0;
+
+    bool is_unit(const std::vector<lit_t> &variables) const noexcept {
+        return variables[get_var(w1)] == 0 && variables[get_var(w2)] != 0;
     }
-    bool is_empty(const std::vector<lit_t> &variables) const {
-        if (variables[std::abs(*w1)] != 0) {
-            assert(variables[std::abs(*w2)] != 0);
+
+    bool is_empty(const std::vector<lit_t> &variables) const noexcept {
+        if (variables[get_var(w1)] != 0) {
+            assert(variables[get_var(w2)] != 0);
             return true;
         }
         return false;
     }
+
 private:
-    lit_t* start;
-    lit_t* end;
-    lit_t* w1;
-    lit_t* w2;
+    WatchHandle start, end, w1, w2;
+
 public:
     lit_t satisfied;
 };
@@ -191,7 +242,7 @@ public:
     std::vector<Clause<watch_tag>*> units;
     std::vector<Clause<watch_tag>*> satisfied;
     std::vector<Clause<watch_tag>> clauses;
-    std::vector<lit_t> literals;
+    std::vector<std::pair<lit_t, std::size_t>> literals;
     bool contra;
 };
 
@@ -213,10 +264,6 @@ struct Assignment {
 struct AdjacencyList {
     std::vector<Adjacency> adjacency;
     std::vector<lit_t> adjacency_data;
-};
-
-struct WatchedList {
-    std::vector<std::unordered_set<Clause<watch_tag>*>> watched_at;
 };
 
 template<>
@@ -245,20 +292,23 @@ public:
         return !cnf.contra;
     }
 
-    void update(lit_t variable, lit_t d, bool is_true) {
-        assign.assigned.push_back(variable);
+    void update(std::size_t variable, lit_t d, bool is_true) {
+        assign.assigned.emplace_back(variable);
         assign.unassigned.erase(variable);
         assign.variables[variable] = is_true ? d : -d;
-        auto copy = wch.watched_at[variable];
-        for (auto &&clause : copy) {
+        auto end = wch.watched_at[variable].end();
+        for (auto it = wch.watched_at[variable].begin(); it != end; ++it) {
+            auto clause = *it;
+
             if (clause->satisfied > 0)
                 continue;
-            clause->assure_watch(variable);
-            bool pos = (clause->snd_watch() > 0) ? is_true : !is_true;
 
-            if (pos || clause->update(assign.variables, wch.watched_at, variable)) {
+            clause->assure_watch(variable);
+            bool sat = (clause->snd_watch() > 0) ^ !is_true;
+
+            if (sat || clause->update(assign.variables, wch)) {
                 assert(clause->satisfied == 0);
-                cnf.satisfied.push_back(clause);
+                cnf.satisfied.emplace_back(clause);
                 clause->satisfied = d;
                 continue;
             }
@@ -268,19 +318,23 @@ public:
                 cnf.contra = true;
                 break;
             }
+
+            assert(std::abs(clause->get_watch()) != variable);
+            if (std::abs(clause->snd_watch()) != variable) {
+                --it;
+                --end;
+            }
         }
     }
 
     bool solve(lit_t d) {
-        if (!unit_propag(d)) {
+        if (!unit_propag(d))
             return false;
-        }
 
         if (assign.unassigned.empty())
             return true;
 
-        auto var = assign.unassigned.begin();
-        auto val = *var;
+        auto val = *assign.unassigned.begin();
 
         update(val, d + 1, true);
 
@@ -299,7 +353,7 @@ public:
 
     void rollback(std::size_t d) {
         for (; !assign.assigned.empty(); assign.assigned.pop_back()) {
-            auto var = assign.assigned.back();
+            auto &&var = assign.assigned.back();
             assert(var != 0);
             auto &&val = assign.variables[var];
             assert(val != 0);
@@ -311,7 +365,7 @@ public:
         }
 
         for (; !cnf.satisfied.empty(); cnf.satisfied.pop_back()) {
-            auto var = cnf.satisfied.back();
+            auto &&var = cnf.satisfied.back();
             assert(var != nullptr);
             auto &&val = var->satisfied;
             assert(val != 0);
@@ -347,7 +401,7 @@ public:
             ++c_counter;
 
             for (auto &&l : c) {
-                cnf.literals[l_counter++] = l;
+                cnf.literals[l_counter++] = {l, 0};
                 std::size_t var = (l > 0) ? l : -l;
                 if (var >= wch.watched_at.size()) {
                     wch.watched_at.resize(2 * var);
@@ -357,7 +411,7 @@ public:
                 assign.unassigned.emplace(var);
             }
 
-            new (cnf.clauses.data() + c_counter) Clause<watch_tag>(cnf.literals.data() + clause_begin, cnf.literals.data() + l_counter, wch.watched_at);
+            new (cnf.clauses.data() + c_counter) Clause<watch_tag>(cnf.literals.data() + clause_begin, cnf.literals.data() + l_counter, wch);
 
             if (clause_begin == l_counter)
                 exit(20);
@@ -371,7 +425,10 @@ public:
     Cnf<nowatch_tag> cnf;
     Assignment assign;
     AdjacencyList adj;
-    void update(lit_t variable, bool is_true) {
+    void update(lit_t variable, lit_t d, bool is_true) {
+        assign.assigned.push_back(variable);
+        assign.unassigned.erase(variable);
+        assign.variables[variable] = is_true ? d : -d;
         if (is_true) {
             for (auto &&a : adj.adjacency[variable]) {
                 if (a > 0) {
@@ -455,15 +512,9 @@ public:
             auto l = *clause.begin();
 
             if (l > 0) {
-                assign.assigned.push_back(l);
-                assign.unassigned.erase(l);
-                assign.variables[l] = d;
-                update(l, true);
+                update(l, d, true);
             } else {
-                assign.assigned.push_back(-l);
-                assign.unassigned.erase(-l);
-                assign.variables[-l] = -d;
-                update(-l, false);
+                update(-l, d, false);
             }
         }
 
@@ -482,42 +533,16 @@ public:
 
         auto var = assign.unassigned.begin();
         auto val = *var;
-        assign.unassigned.erase(var);
-        std::size_t score[] = {0, 0};
-        for (auto &&c : adj.adjacency[val])
-            ++score[(c > 0) ? 0 : 1];
+        update(val, d + 1, true);
 
-        if (score[0] + score[1] == 0) {
-            assign.assigned.push_back(val);
-            assign.variables[val] = 0;
-            return solve(d);
-        }
+        if (solve(d + 1))
+            return true;
+        else
+            rollback(d);
 
-        if (score[0] > 0) {
-            assign.assigned.push_back(val);
-            assign.variables[val] = d + 1;
-            update(val, true);
+        update(val, d + 1, false);
 
-            if (solve(d + 1)) {
-                return true;
-            } else {
-                rollback(d);
-
-                if (score[1] > 0)
-                    assign.unassigned.erase(val);
-            }
-        }
-
-        if (score[1] > 0) {
-            assign.assigned.push_back(val);
-            assign.variables[val] = -(d + 1);
-            update(val, false);
-
-            if (solve(d + 1))
-                return true;
-        }
-
-        return false;
+        return solve(d + 1);
     }
 
     void set(std::vector<std::vector<lit_t>> &list) {
