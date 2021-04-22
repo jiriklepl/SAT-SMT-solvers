e28b6b5aaaf06433e5c7f3e7434086adcbd8a20e
jiriklepl <jiriklepl@seznam.cz>
2021-04-22 02:04:14 +0200

    improved the test file

--- sat-use/backbones/test.sh
+++ sat-use/backbones/test.sh
@@ -1,15 +1,37 @@
 #!/bin/sh
-SOLUTIONS="minibones\nlglbones"
+SOLUTIONS="glucobones
+lglbones
+minibones
+cadibones"
 B_NUMS="10\n30"
-DIR="data/cbs_b"
 
 make
 
+[ -d "out" ] || mkdir "out"
+
 for sol in `echo "$SOLUTIONS"`; do
-    for num in `echo "$B_NUMS"`; do
-        for file in `ls "$DIR$num"`; do
-            lines=$(expr `./$sol "$DIR$num/$file" | wc -l` - 1)
-            echo $lines
-        done
+    printf "" > "out/$sol.csv"
+    for dir in `find data -maxdepth 1 -mindepth 1 -type d`; do
+        for file in `ls "$dir/"`; do
+            echo "$dir" | sed "s/.*m\([0-9]\+\).*b\([0-9]\+\).*/\1 \2/" | tr "\n" " "
+            "./$sol" "$dir/$file" | grep -v "^c" | awk "
+NR == 1 {
+    time = \$1
+}
+NR == 2 {
+    # nbones = \$1
+    calls = \$4
+}
+# NR >= 3 {
+#     if (\$2 == \"true\")
+#         print substr(\$1, 1, length(\$1) - 1)
+#     if (\$2 == \"true\")
+#         print \"¬\" substr(\$1, 1, length(\$1) - 1)
+# }
+END {
+    print time \" \" calls
+}
+"
+        done >>  "out/$sol.csv"
     done
 done
