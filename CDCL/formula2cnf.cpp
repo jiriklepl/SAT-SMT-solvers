d461854c713ae1de346618566822e903f5a5cf7d
jiriklepl <jiriklepl@seznam.cz>
2021-04-25 00:37:50 +0200

    added the io boilerplate

--- CDCL/formula2cnf.cpp
+++ CDCL/formula2cnf.cpp
@@ -2,25 +2,70 @@
 #include <cassert>
 #include <fstream>
 #include <iostream>
-#include <utility>
+#include <optional>
 #include <string>
+#include <utility>
 
 #include "satlib.hpp"
 
+using namespace std::string_literals;
+
+void usage(std::ostream &out) {
+    out << "Usage: formula2cnf [-e|--eq] [input [output]]" << std::endl;
+}
+
 int main(int argc, const char *argv[])
 {
+    std::optional<std::ifstream> input;
+    std::optional<std::ofstream> output;
     std::unique_ptr<satlib_parser> parser;
-    bool just_impl = false;
+    std::size_t parsed_files = 0;
+    const char *input_name = "/dev/stdin";
+    bool just_impl = true;
 
-    if (argc == 2) {
-        auto file = std::ifstream(argv[1]);
-        parser = satlib_parse(file, just_impl);
-    } else {
-        parser = satlib_parse(std::cin, just_impl);
+    for (const char** arg = argv + 1; *arg != nullptr; ++arg) {
+        if (**arg == '-') {
+            if (*arg == "-e"s || *arg == "--eq"s) {
+                just_impl = false;
+            } else {
+                std::cerr << "Unknown option: " << *arg << std::endl;
+                usage(std::cerr);
+                exit(1);
+            }
+
+            continue;
+        }
+
+        switch (parsed_files++) {
+        case 0:
+            input = std::ifstream(input_name = *arg);
+            if (!input->good()) {
+                std::cerr << "Failed to open file: " << *arg << std::endl;
+                exit(2);
+            }
+        break;
+        case 1:
+            output = std::ofstream(*arg);
+            if (!output->good()) {
+                std::cerr << "Failed to open file: " << *arg << std::endl;
+                exit(2);
+            }
+        break;
+        default:
+            usage(std::cerr);
+            exit(1);
+        }
+
     }
 
+    parser = satlib_parse(input.has_value() ? *input : std::cin, just_impl);
+
+    std::ostream &out = output.has_value() ? *output : std::cout;
+
+    out << "c file "  << input_name << std::endl;
+
     if (parser->cnf.empty()) {
-        std::cout << "s TAUTOLOGY" << std::endl;
+        out << "p cnf 0 0" << std::endl;
         return 10;
     }
 
@@ -32,7 +77,7 @@ int main(int argc, const char *argv[])
     for (auto &&[name, var] : parser->names) {
         assert(var > 0);
 
-        std::cout << "c variable " << var << " : " << name << std::endl;
+        out << "c variable " << var << " : " << name << std::endl;
 
         if (!just_impl)
             variables.push_back(var);
@@ -52,7 +97,7 @@ int main(int argc, const char *argv[])
             assert(clause[0] < 0);
             assert(clause.size() == 2);
 
-            std::cout << " and " << clause[1] << std::endl;
+            out << " and " << clause[1] << std::endl;
 
             finish_and = false;
             continue;
@@ -66,23 +111,23 @@ int main(int argc, const char *argv[])
 
         // if just_impl, `clause[0]` has to be a gate, otherwise we check whether it is really a gate
         if (just_impl || !std::binary_search(variables.begin(), variables.end(), -clause[0])) {
-            std::cout <<  "c gate " << -clause[0] << " : " << clause[1];
+            out <<  "c gate " << -clause[0] << " : " << clause[1];
 
             if (clause.size() == 2)
                 finish_and = true;
             else
-                std::cout << " or " << clause[2] << std::endl;
+                out << " or " << clause[2] << std::endl;
         }
     }
 
     assert(parser->cnf.back().size() == 1);
-    std::cout << "c root " << parser->cnf.back()[0] << std::endl;
+    out << "c root " << parser->cnf.back()[0] << std::endl;
 
-    std::cout << "p cnf " << parser->var_counter << ' ' << parser->cnf.size() << std::endl;
+    out << "p cnf " << parser->var_counter << ' ' << parser->cnf.size() << std::endl;
 
     for (auto &&clause : parser->cnf) {
         for (auto &&lit : clause)
-            std::cout << lit << ' ';
-        std::cout << '0' << std::endl;
+            out << lit << ' ';
+        out << '0' << std::endl;
     }
 }
