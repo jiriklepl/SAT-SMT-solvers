ca0dbfe60e64ce5358d0ca74249a616797660f85
jiriklepl <jiriklepl@seznam.cz>
2021-05-20 02:48:40 +0200

    added more options, added more detailed help

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <optional>
 #include <string>
+#include <sstream>
 #include <unordered_set>
 
 #include "dimacs.hpp"
@@ -14,8 +15,23 @@
 #include "watched_sep_cdcl.hpp"
 #include "adjacency.hpp"
 
-void usage(std::ostream &out) {
-    out << "Usage: dpll [Options] [input [output]]" << std::endl;
+void usage(const char *name, std::ostream &out) {
+    out << "Usage: " << name << " [Options] [input [output]]" << std::endl << std::endl;
+
+    out << "Valid options:" << std::endl;
+    out << "  -e, --equiv: for tseitsing encoding of satlib formulas, use equivalences" << std::endl;
+    out << "  -c, --dimacs: the input is in the dimacs format (for files, regardless of the file extension)" << std::endl;
+    out << "  -s, --satlib: the input is in the satlib format (for files, regardless of the file extension)" << std::endl;
+    out << "  -h, --help: print this usage help" << std::endl;
+    out << "  -l LIMIT, --cache-limit LIMIT: set the initial cache LIMIT for learned clauses (relevant only in the CDCL_WATCHED_SEP mode)" << std::endl;
+    out << "  -u LENGTH, --cache-limit LENGTH: set the unit run LENGTH (where LENGTH is a positive number) which acts as a multiplicator for the number of contradictions between subsequent restarts (relevant only in the CDCL_WATCHED or CDCL_WATCHED_SEP mode)" << std::endl << std::endl;
+
+    out << "  mode modifiers: (the default mode is CDCL_WATCHED_SEP)" << std::endl;
+    out << "    -a,   --adjacency: switches the parser to the ADJACENCY mode" << std::endl;
+    out << "    -w,   --watched: switches the parser to the WATCHED mode" << std::endl;
+    out << "    -ws,  --watched-sep: switches the parser to the WATCHED_SEP mode" << std::endl;
+    out << "    -cw,  --cdcl-watched: switches the parser to the CDCL_WATCHED mode" << std::endl;
+    out << "    -cws, --cdcl-watched-sep: switches the parser to the CDCL_WATCHED_SEP mode" << std::endl;
 }
 
 using namespace std::string_literals;
@@ -27,16 +43,21 @@ int main(int, const char *argv[])
     std::unique_ptr<satlib_parser> parser;
     std::size_t parsed_files = 0;
     const char *input_name = "/dev/stdin";
+
     bool just_impl = true;
     bool forced_dimacs = false;
     bool forced_satlib = false;
+
+    int unit_run = 100;
+    std::size_t cache_limit = 10'000;
+
     enum {
         ADJACENCY,
         WATCHED,
         WATCHED_SEP,
         CDCL_WATCHED_SEP,
         CDCL_WATCHED,
-    } solver_type = CDCL_WATCHED;
+    } solver_type = CDCL_WATCHED_SEP;
 
     for (const char** arg = argv + 1; *arg != nullptr; ++arg) {
         if (**arg == '-') {
@@ -49,19 +70,37 @@ int main(int, const char *argv[])
                 forced_dimacs = false;
                 forced_satlib = true;
             } else if (*arg == "-h"s || *arg == "--help"s) {
-                usage(std::cout);
+                usage(*argv, std::cout);
                 return 0;
+            } else if (*arg == "-l"s || *arg == "--cache-limit"s) {
+                ++arg;
+
+                if (*arg == nullptr) {
+                    usage(*argv, std::cerr);
+                    return 1;
+                }
+
+                std::istringstream(*arg) >> cache_limit;
+            } else if (*arg == "-u"s || *arg == "--unit-run"s) {
+                ++arg;
+
+                if (*arg == nullptr || (std::istringstream(*arg) >> unit_run, unit_run) <= 0) {
+                    usage(*argv, std::cerr);
+                    return 1;
+                }
             } else if (*arg == "-a"s || *arg == "--adjacency"s) {
                 solver_type = ADJACENCY;
             } else if (*arg == "-w"s || *arg == "--watched"s) {
                 solver_type = WATCHED;
-            } else if (*arg == "-ws"s || *arg == "--watched_sep"s) {
+            } else if (*arg == "-ws"s || *arg == "--watched-sep"s) {
                 solver_type = WATCHED_SEP;
-            } else if (*arg == "-cs"s || *arg == "--watched_cdcl_sep"s) {
+            } else if (*arg == "-cw"s || *arg == "--cdcl-watched"s) {
+                solver_type = CDCL_WATCHED;
+            } else if (*arg == "-cws"s || *arg == "--cdcl-watched-sep"s) {
                 solver_type = CDCL_WATCHED_SEP;
             } else {
                 std::cerr << "Unknown option: " << *arg << std::endl;
-                usage(std::cerr);
+                usage(*argv, std::cerr);
                 return 1;
             }
 
@@ -71,20 +110,24 @@ int main(int, const char *argv[])
         switch (parsed_files++) {
         case 0:
             input = std::ifstream(input_name = *arg);
+
             if (!input->good()) {
                 std::cerr << "Failed to open file: " << *arg << std::endl;
+
                 return 2;
             }
         break;
         case 1:
             output = std::ofstream(*arg);
+
             if (!output->good()) {
                 std::cerr << "Failed to open file: " << *arg << std::endl;
+
                 return 2;
             }
         break;
         default:
-            usage(std::cerr);
+            usage(*argv, std::cerr);
             return 1;
         }
 
@@ -103,10 +146,10 @@ int main(int, const char *argv[])
         solver = std::make_unique<SolverWatchedSep>();
     break;
     case CDCL_WATCHED_SEP:
-        solver = std::make_unique<SolverWatchedSepCDCL>();
+        solver = std::make_unique<SolverWatchedSepCDCL>(unit_run, cache_limit);
     break;
     case CDCL_WATCHED:
-        solver = std::make_unique<SolverWatchedCDCL>();
+        solver = std::make_unique<SolverWatchedCDCL>(unit_run);
     break;
     }
 
