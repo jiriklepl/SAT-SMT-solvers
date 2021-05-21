4b13d0b1ba4f509e783c12041db530368e230049
jiriklepl <jiriklepl@seznam.cz>
2021-05-21 18:22:43 +0200

    printing aggregated results

--- CPP/script.R
+++ CPP/script.R
@@ -6,8 +6,10 @@ options(tibble.print_max = Inf)
 
 col_names <- c("type", "variables", "id", "sat", "time", "decisions", "derivations")
 
-data <- do.call("rbind", lapply(list.files("out", full.names=TRUE), function(x) return (read_csv(x, col_names=col_names) %>% filter(type == "uf") %>%
-    group_by(variables) %>% summarize(name=x, time = mean(time), decisions = mean(decisions), derivations = mean(derivations)))))
+data <- do.call("rbind", lapply(list.files("out", full.names=TRUE), function(x) return (read_csv(x, col_names=col_names)  %>%
+    group_by(type, variables) %>% summarize(.groups="keep", name=x, time = mean(time), decisions = mean(decisions), derivations = mean(derivations)))))
 
-data %>% filter(variables == 50) %>% arrange(time)
-data %>% filter(variables == 100) %>% arrange(time)
+data %>% filter(variables == 50) %>% arrange(type, time)
+data %>% filter(variables == 100) %>% arrange(type, time)
+data %>% filter(variables == 125) %>% arrange(type, time)
+data %>% filter(variables == 150) %>% arrange(type, time)
