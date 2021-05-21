0405d333505eae51f6693653110c29bab0ad3c86
jiriklepl <jiriklepl@seznam.cz>
2021-05-21 05:42:39 +0200

    better R script

--- CPP/script.R
+++ CPP/script.R
@@ -2,9 +2,12 @@
 
 library('tidyverse')
 
-adjacency <- read_csv("out/adjacency.csv", col_names=c("variables", "id", "time", "decisions", "derivations")) %>%
-    group_by(variables) %>% summarize(time = mean(time), decisions = mean(decisions), derivations = mean(derivations))
-watched <- read_csv("out/watched.csv", col_names=c("variables", "id", "time", "decisions", "derivations")) %>%
-    group_by(variables) %>% summarize(time = mean(time), decisions = mean(decisions), derivations = mean(derivations)) %>% mutate(variables=1)
+options(tibble.print_max = Inf)
 
-adjacency / watched
+col_names <- c("type", "variables", "id", "sat", "time", "decisions", "derivations")
+
+data <- do.call("rbind", lapply(list.files("out", full.names=TRUE), function(x) return (read_csv(x, col_names=col_names) %>% filter(type == "uf") %>%
+    group_by(variables) %>% summarize(name=x, time = mean(time), decisions = mean(decisions), derivations = mean(derivations)))))
+
+data %>% filter(variables == 50) %>% arrange(time)
+data %>% filter(variables == 100) %>% arrange(time)
