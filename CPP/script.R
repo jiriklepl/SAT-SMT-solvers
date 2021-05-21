0a57831c6944c0feb9fd8b121d8eee3792077654
jiriklepl <jiriklepl@seznam.cz>
2021-05-21 19:14:05 +0200

    better r script

--- CPP/script.R
+++ CPP/script.R
@@ -9,7 +9,4 @@ col_names <- c("type", "variables", "id", "sat", "time", "decisions", "derivatio
 data <- do.call("rbind", lapply(list.files("out", full.names=TRUE), function(x) return (read_csv(x, col_names=col_names)  %>%
     group_by(type, variables) %>% summarize(.groups="keep", name=x, time = mean(time), decisions = mean(decisions), derivations = mean(derivations)))))
 
-data %>% filter(variables == 50) %>% arrange(type, time)
-data %>% filter(variables == 100) %>% arrange(type, time)
-data %>% filter(variables == 125) %>% arrange(type, time)
-data %>% filter(variables == 150) %>% arrange(type, time)
+data %>% arrange(type, variables, time)
